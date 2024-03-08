010f076e7f5d70d4a9cdba742f2682040c69f68e
Hima <asondos867@gmail.com>
2024-03-08 03:40:39 +0200

    deployieng

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,54 +1,88 @@
-# Ensure /data directory exists
-file { '/data':
-  ensure => 'directory',
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
+# AirBnB clone web server setup and configuration
+
+# SCRIPT INCOMPLETE. NEEDS SOME MORE THINKING---
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://linktr.ee/firdaus_h_salim/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
 }
 
-# Ensure /data/web_static directory structure exists
-file { '/data/web_static':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
+-> file { '/data':
+  ensure  => 'directory'
 }
 
-file { '/data/web_static/releases':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
+-> file { '/data/web_static':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/shared':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
 }
 
-file { '/data/web_static/releases/test':
-  ensure => 'directory',
-  owner  => 'root',
-  group  => 'root',
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
 }
 
-# Create index.html file
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => '<html><head></head><body>Holberton School</body></html>',
-  owner   => 'root',
-  group   => 'root',
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
 }
 
-# Create symbolic link
-file { '/data/web_static/current':
-  ensure  => 'link',
-  target  => '/data/web_static/releases/test',
-  owner   => 'root',
-  group   => 'root',
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
 }
 
-# Restart Nginx service (assuming Nginx is managed by Puppet)
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
-  require => File['/data/web_static/current'],
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+}
+
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+
+file { '/var/www':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+
+-> file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
+
+-> file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page - Error page\n"
+}
+
+-> file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+}
+
+-> exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
