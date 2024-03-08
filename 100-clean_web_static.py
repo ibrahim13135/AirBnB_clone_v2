65449970b636316d8ef304253fd279331ec55291
Hima <asondos867@gmail.com>
2024-03-08 03:39:41 +0200

    """

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,44 +1,33 @@
 #!/usr/bin/python3
 """
-Fabric script (based on the file 3-deploy_web_static.py) that deletes
-out-of-date archives, using the function do_clean.
+Deletes out-of-date archives
+fab -f 100-clean_web_static.py do_clean:number=2
+    -i ssh-key -u ubuntu > /dev/null 2>&1
 """
 
-from fabric.api import *
 import os
+from fabric.api import *
 
-env.hosts = ['<IP web-01>', '<IP web-02>']
+env.hosts = ['52.72.74.64', '3.90.80.21']
 
 
 def do_clean(number=0):
+    """Delete out-of-date archives.
+    Args:
+        number (int): The number of archives to keep.
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
     """
-    Deletes out-of-date archives
-    """
+    number = 1 if int(number) == 0 else int(number)
 
-    try:
-        number = int(number)
-    except ValueError:
-        print("Invalid number. Please provide a valid integer.")
-        return
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
 
-    if number < 2:
-        number = 1
-
-    with lcd('versions'):
-        local('ls -t | tail -n +{} | xargs -I {{}} rm {{}}'.format(number))
-
-    with cd('/data/web_static/releases'):
-        run('ls -t | tail -n +{} | xargs -I {{}} rm -rf {{}}'.format(number))
-
-
-# Check if executed as standalone script
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(
-        description="Fabric script to delete out-of-date archives")
-    parser.add_argument("number", type=int, nargs="?", default=0,
-                        help="Number of archives to keep, including the most recent. Default is 0.")
-    args = parser.parse_args()
-
-    do_clean(args.number)
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
