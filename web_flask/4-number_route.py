8550f352d3ba0514de506060b041bb528f60b61c
Hima <asondos867@gmail.com>
2024-03-24 14:51:38 +0200

    modify flask

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,7 +28,7 @@ def c_text(text):
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
-    """Route that displays 'Python ' followed by the value of the text variable"""
+    """Route that displays 'Python '"""
     return "Python " + text.replace('_', ' ')
 
 
