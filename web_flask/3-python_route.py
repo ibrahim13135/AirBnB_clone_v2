fbfbc6f323a7405aa54e5d89914949d42dfe7c20
Hima <asondos867@gmail.com>
2024-03-24 15:22:27 +0200

    si

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
+""" 3. Add third view func that redirects and has default val for variable """
+
 from flask import Flask
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return "Hello HBNB!"
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    return "HBNB"
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route('/c/<text>')
 def c_text(text):
-    return "C " + text.replace('_', ' ')
+    """ replace text with variable. """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
 
-@app.route('/python/<text>', strict_slashes=False)
-def py_text(text):
-    return "Python " + text.replace('_', ' ')
 
-if __name__ == "__main__":
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """ replace more text with another variable. """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
