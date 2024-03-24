334c45456665f9485ca0ba935e580439906bfd91
Hima <asondos867@gmail.com>
2024-03-24 13:15:17 +0200

    No meaningful commit message provided

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,14 +8,8 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Route that displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """Route that displays 'HBNB'"""
-    return "HBNB"
+    return "<h1>Hello HBNB!</h1>"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
