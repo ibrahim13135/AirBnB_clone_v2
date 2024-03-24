1ecabec4d144001bc20281e91de04e355276cf93
Hima <asondos867@gmail.com>
2024-03-24 22:34:23 +0200

    modify Flask

--- models/state.py
+++ models/state.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
 """This is the state class"""
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+import os
 import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """This is the class for State
     Attributes:
         name: input name
+        cities = relationship between state and city tables.
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete-orphan',
-                          backref="state", passive_deletes=True)
 
-    @property
-    def cities(self):
-        """Getter method to return the list of City objects linked to the current State"""
-        city_list = []
-        if models.storage.__class__.__name__ != "DBStorage":
-            for city in models.storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-        return city_list
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            'City', back_populates='state',
+            cascade='all, delete, delete-orphan')
+
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """returns list of Cities and some relationships"""
+            cities_instances = []
+            cities_dict = models.storage.all(models.City)
+            for key, value in cities_dict.items():
+                if self.id == value.state_id:
+                    cities_instances.append(value)
+            return cities_instances
